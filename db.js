7ec7a0ff41fe948a28be211d5c31f76b08d5833a
Jatin <jatin333x@gmail.com>
2025-12-18 18:00:24 +0530

    shift mongodb to online server

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const mongoDBURL = 'mongodb://127.0.0.1:27017/firstdb';
+// const mongoDBURL = 'mongodb://127.0.0.1:27017/firstdb';
+const mongoDBURL = "mongodb+srv://helloworld:QWERTY123456@cluster0.9od9uxl.mongodb.net/"
 
 const connectDB = async () => {
     try{
