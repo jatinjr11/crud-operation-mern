eccb8ff0f3eb4e194e9383989d4fc496295cf573
Jatin <jatin333x@gmail.com>
2025-12-22 19:50:26 +0530

    authentication and authorization

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 require("dotenv").config();
-// const MONGODB_URL_LOCAL = process.env.MONGODB_URL_LOCAL;
-const mongoDBURL = process.env.DB_URL;
+const MONGODB_URL_LOCAL = process.env.MONGODB_URL_LOCAL;
+// const mongoDBURL = process.env.DB_URL;
 
 const connectDB = async () => {
     try{
-        await mongoose.connect(mongoDBURL);
+        await mongoose.connect(MONGODB_URL_LOCAL);
         console.log("MongoDB DB connected Successfully");
 
         mongoose.connection.on('error', (err) => {
--- index.js
+++ index.js
@@ -6,16 +6,28 @@ const personRoutes = require('./routes/personRoute');
 const menuItemRoutes = require('./routes/menuItemRoutes');
 require('dotenv').config();
 const PORT = process.env.PORT || 8000;
-
+const passport = require("./auth");
 
 app.use(bodyParser.json());
-
 db();
 
-app.use('/person', personRoutes);
+// middleware functions
+const logRequest = (req, res, next) => {
+    console.log(`[${new Date().toLocaleString()}] Request Made to: ${req.originalUrl}`);
+    next(); // move on to the next phase
+}
+app.use(logRequest);
+
+
+
+app.use(passport.initialize());
+const localAuthMiddleware = passport.authenticate('local', {session: false}) ; 
+
+app.use('/person', localAuthMiddleware, personRoutes);
 app.use('/menu', menuItemRoutes)
 
-app.get('/', (req, res) => {
+
+app.get('/',localAuthMiddleware , (req, res) => {
     res.send("hello jiii")
 });
 
